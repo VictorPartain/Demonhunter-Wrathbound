ba2b2e038081fb24e888c864db19e1c74b41d7c8
VictorPartain <victorpartain@gmail.com>
2025-05-23 21:17:22 -0700

    Got metamorphosis to work and now I just have to fix the bug of it removing itself when i go near a hostile mob

--- src/main/java/victorstone/demonhunter/effect/MetamorphosisStatusEffect.java
+++ src/main/java/victorstone/demonhunter/effect/MetamorphosisStatusEffect.java
@@ -1,8 +1,11 @@
 package victorstone.demonhunter.effect;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.util.Identifier;
 import net.spell_engine.api.spell.fx.ParticleBatch;
 import net.spell_engine.fx.ParticleHelper;
 
@@ -10,6 +13,26 @@ public class MetamorphosisStatusEffect extends StatusEffect {
     public static final float multiplier = 0.5f;
     public MetamorphosisStatusEffect(StatusEffectCategory statusEffectCategory, int color) {
         super(statusEffectCategory, color);
+
+
+        this.addAttributeModifier(
+                EntityAttributes.GENERIC_MAX_HEALTH,
+                Identifier.of("demonhunter:metamorphosis_max_health"),
+                10,
+                EntityAttributeModifier.Operation.ADD_VALUE);
+
+        this.addAttributeModifier(
+                EntityAttributes.GENERIC_ARMOR,
+                Identifier.of("demonhunter:metamorphosis_armor"),
+                4.0,
+                EntityAttributeModifier.Operation.ADD_VALUE
+                );
+        this.addAttributeModifier(
+                EntityAttributes.GENERIC_ATTACK_DAMAGE,
+                Identifier.of("demonhunter:metamorphosis_dmg"),
+                5.0,
+                EntityAttributeModifier.Operation.ADD_VALUE
+        );
     }
     private static final ParticleBatch particles = new ParticleBatch(
             "spell_engine:holy_spark",
@@ -24,4 +47,16 @@ public class MetamorphosisStatusEffect extends StatusEffect {
     public static void pop(Entity centerEntity) {
         ParticleHelper.sendBatches(centerEntity, new ParticleBatch[]{particles});
     }
+//    @Override
+//    public void onApplied(net.minecraft.entity.LivingEntity entity, net.minecraft.entity.attribute.AttributeContainer attributes, int amplifier) {
+//        // Apply absorption
+//        entity.setAbsorptionAmount(4 + amplifier * 2);
+//    }
+//
+//    @Override
+//    public void onRemoved(net.minecraft.entity.LivingEntity entity, net.minecraft.entity.attribute.AttributeContainer attributes, int amplifier) {
+//        // Remove absorption
+//        entity.setAbsorptionAmount(0);
+//    }
+
 }
